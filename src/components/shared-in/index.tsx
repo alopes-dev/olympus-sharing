7392f1e5587103d20e021601eda59df3dbec393c
Antonio Lopes-(alopes.dev) <pdkdidy2011@gmail.com>
2024-05-04 13:37:22 +0100

    feat: configs

--- src/components/shared-in/index.tsx
+++ src/components/shared-in/index.tsx
@@ -1,8 +1,8 @@
-import { getSharedOlympus } from "@root/services/get-shared-olympus";
-import { TOlympus } from "@root/types";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
-import { ScrollView, View } from "react-native";
-import { AppPlaceholderCard } from "../_shared";
+import {getSharedOlympus} from "@root/services/get-shared-olympus";
+import {TOlympus} from "@root/types";
+import React, {useCallback, useEffect, useMemo, useState} from "react";
+import {ScrollView, View} from "react-native";
+import {AppPlaceholderCard} from "../_shared";
 
 import SharedInHeader from "./shared-in-header";
 import SharedInOnlineOlympus from "./shared-in-online-olympus";
@@ -10,58 +10,58 @@ import * as S from "./shared-in.styles";
 import SharedOlympus from "./shared-olympus";
 
 const SharedIn = () => {
-  const [sharedOlympus, setSharedOlympus] = useState<TOlympus[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+    const [sharedOlympus, setSharedOlympus] = useState<TOlympus[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-  const handleGetSharedOlympus = useCallback(async () => {
-    setIsLoading(true);
-    const sharedOlympusResult = await getSharedOlympus();
-    setSharedOlympus(sharedOlympusResult ?? []);
-    setIsLoading(false);
-  }, []);
+    const handleGetSharedOlympus = useCallback(async () => {
+        setIsLoading(true);
+        const sharedOlympusResult = await getSharedOlympus();
+        setSharedOlympus(sharedOlympusResult ?? []);
+        setIsLoading(false);
+    }, []);
 
-  useEffect(() => {
-    handleGetSharedOlympus();
-  }, [handleGetSharedOlympus]);
+    useEffect(() => {
+        handleGetSharedOlympus()
+    }, [handleGetSharedOlympus]);
 
-  const onlineOlympus = useMemo(
-    () => sharedOlympus?.filter((olympus) => olympus.isOnline),
-    [sharedOlympus]
-  );
+    const onlineOlympus = useMemo(
+        () => sharedOlympus?.filter((olympus) => olympus.isOnline),
+        [sharedOlympus]
+    );
 
-  return (
-    <S.Root>
-      <SharedInHeader
-        totalShared={sharedOlympus?.length ?? 0}
-        totalOnline={onlineOlympus?.length ?? 0}
-      />
-      <ScrollView>
-        <SharedInOnlineOlympus
-          isLoading={isLoading}
-          onlineOlympus={onlineOlympus}
-        />
-        <S.SharedOlympusTitle>Shared Olympus</S.SharedOlympusTitle>
-
-        {isLoading && (
-          <View style={{ paddingHorizontal: 16 }}>
-            <AppPlaceholderCard
-              count={10}
-              variant="dark"
-              style={{
-                width: "100%",
-                height: 50,
-                marginBottom: 16,
-              }}
+    return (
+        <S.Root>
+            <SharedInHeader
+                totalShared={sharedOlympus?.length ?? 0}
+                totalOnline={onlineOlympus?.length ?? 0}
             />
-          </View>
-        )}
+            <ScrollView>
+                <SharedInOnlineOlympus
+                    isLoading={isLoading}
+                    onlineOlympus={onlineOlympus}
+                />
+                <S.SharedOlympusTitle>Shared Olympus</S.SharedOlympusTitle>
 
-        {sharedOlympus?.map((olympus) => (
-          <SharedOlympus olympus={olympus} key={Math.random().toString()} />
-        ))}
-      </ScrollView>
-    </S.Root>
-  );
+                {isLoading && (
+                    <View style={{paddingHorizontal: 16}}>
+                        <AppPlaceholderCard
+                            count={10}
+                            variant="dark"
+                            style={{
+                                width: "100%",
+                                height: 50,
+                                marginBottom: 16,
+                            }}
+                        />
+                    </View>
+                )}
+
+                {sharedOlympus?.map((olympus) => (
+                    <SharedOlympus olympus={olympus} key={Math.random().toString()}/>
+                ))}
+            </ScrollView>
+        </S.Root>
+    );
 };
 
 export default SharedIn;
